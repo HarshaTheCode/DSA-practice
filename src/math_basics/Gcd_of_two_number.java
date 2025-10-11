c4b43f2bf9fa97871d43077fb3128ba7ee6fc944
HarshaTheCode <harshapc0109@gmail.com>
2025-10-11 20:55:16 +0530

    Euclidean  algo

--- src/math_basics/Gcd_of_two_number.java
+++ src/math_basics/Gcd_of_two_number.java
@@ -2,28 +2,20 @@ package math_basics;
 
 public class Gcd_of_two_number {
 	
-	  public static int GCD(int n1, int n2) {
+	  public static int GCD(int a, int b) {
 		  
-		  int some=0;
-		  if (n2>n1) {
-		  for (int i=1; i<n2;i++) {
-			  if (n1%i==0 && n2%i==0) {
-				  some=i;
-			  }
-		  }
-		  }else {
-			  for(int i=1;i<n1;i++) {
-				  if (n1%i==0 && n2%i==0) {
-					  some=i; 
-				  }			  
-			  }
-			  
-		  }
-		  System.out.println(some);
-		  return some;
-	    } 
-	  
+	    
+	 while (a>0&&b>0) {
+		 if(a>b) {
+			 a=a%b;
+		 }else {
+			 b=b%a;
+		 }
+	 }
+	 if (a==0) return b;
+	  return a;
+	  } 
 	public static void main(String[] args) {
-	 	GCD(20,10);
+	 	System.out.println(GCD(6,9));
 	}
 }
